6c3633f56f647111aaf5ffb7eeb5e39d12a0b987
Maisie Klaassen <maisie.c.klaassen@gmail.com>
2021-11-21 15:34:46 +0100

    Activated inverse kinematics

--- project/assets/models/creatures/spidery/positioning/positioning.gd
+++ project/assets/models/creatures/spidery/positioning/positioning.gd
@@ -4,14 +4,20 @@ onready var follow_raycast : RayCast = null
 
 var position_on_curve := 0.0
 var position_start := Vector3.ZERO
+var position_current := global_transform.origin
 var position_end := Vector3.ZERO
+var name_raycast := "no name given"
 
 var move_position := false
 
 func _ready():
 	set_as_toplevel(true)
 
-func _process(delta):
+func _physics_process(_delta: float) -> void:
+	
+	#print(name_raycast +" - "+ str(global_transform.origin.distance_to(follow_raycast.global_transform.origin)))
+	#if raycast.is_colliding():
+		#print(raycast.get_collision_point())
 	position_on_curve += 0.05
 	
 	if position_on_curve >= 1:
@@ -26,8 +32,9 @@ func _move_position(start: Vector3, end: Vector3) -> void:
 	position_end = end
 	move_position = true
 
-func _register_raycast(ray: RayCast) -> void:
+func _register_raycast(ray: RayCast, name_rc: String) -> void:
 	follow_raycast = ray
+	name_raycast = name_rc
 
 func _calculate_bezier_curve(start: Vector3, end: Vector3):	
 	var generate_third_point := Vector3.ZERO;
@@ -39,3 +46,6 @@ func _calculate_bezier_curve(start: Vector3, end: Vector3):
 	var calc_point_1 = generate_third_point.linear_interpolate(end, position_on_curve)
 	var current_position = calc_point_0.linear_interpolate(calc_point_1, position_on_curve)
 	transform.origin = current_position
+
+func _check_distance() -> bool:
+	return global_transform.origin.distance_to(follow_raycast.global_transform.origin) > 3
--- project/scenes/creatures/spidery/spidery.gd
+++ project/scenes/creatures/spidery/spidery.gd
@@ -1,5 +1,24 @@
 extends KinematicBody
 
+#	----------------------
+#	For character movement
+#	----------------------
+export var speed := 3.0
+export var jump_strength := 30.0
+export var gravity := 50
+
+onready var pointFL: Spatial = get_node("pointFL")
+onready var pointFR: Spatial = get_node("pointFR")
+
+var _velocity := Vector3.ZERO
+var _snap_vector := Vector3.DOWN
+
+onready var _spring_arm: SpringArm = $SpringArm
+onready var _model: Spatial = $Armature
+
+#	------------------------------
+#	For inverse kinematic movement
+#	------------------------------
 onready var positionFL := $positions/positionLegFL
 onready var positionFR := $positions/positionLegFR
 onready var positionBL := $positions/positionLegBL
@@ -10,9 +29,49 @@ onready var rayCastFR := $RaycastsLegs/RayCastLegFR
 onready var rayCastBL := $RaycastsLegs/RayCastLegBL
 onready var rayCastBR := $RaycastsLegs/RayCastLegBR
 
-func _ready():
-	positionFL._register_raycast(rayCastFL)
-	positionFR._register_raycast(rayCastFR)
-	positionBL._register_raycast(rayCastBL)
-	positionBR._register_raycast(rayCastBR)
+onready var legFL_IK := $Armature/Skeleton/legFrontLeft
+onready var legFR_IK := $Armature/Skeleton/legFrontRight
+onready var legBL_IK := $Armature/Skeleton/legBackLeft
+onready var legBR_IK := $Armature/Skeleton/legBackRight
 
+func _ready():
+	positionFL._register_raycast(rayCastFL, "FL")
+	positionFR._register_raycast(rayCastFR, "FR")
+	positionBL._register_raycast(rayCastBL, "BL")
+	positionBR._register_raycast(rayCastBR, "BR")
+	
+	legFL_IK.start()	
+	legFR_IK.start()	
+	legBL_IK.start()	
+	legBR_IK.start()	
+
+func _physics_process(delta: float) -> void:
+	move_character(delta)
+
+func _process(_delta: float) -> void:
+	_spring_arm.translation = translation	
+	
+func move_character(delta: float) -> void:
+	var move_direction := Vector3.ZERO
+	move_direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	move_direction.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	move_direction = move_direction.rotated(Vector3.UP, _spring_arm.rotation.y).normalized()
+
+	_velocity.x = move_direction.x * speed
+	_velocity.z = move_direction.z * speed
+	_velocity.y -= gravity * delta
+	
+	var just_landed := is_on_floor() and _snap_vector == Vector3.ZERO
+	var is_jumping := is_on_floor() and Input.is_action_just_pressed("ui_accept")
+	
+	# Set jumping / gravity
+	if is_jumping:
+		_velocity.y = jump_strength
+		_snap_vector = Vector3.ZERO
+	elif just_landed:
+		_snap_vector = Vector3.DOWN
+	_velocity = move_and_slide_with_snap(_velocity, _snap_vector, Vector3.UP, true)
+	
+	if _velocity.length() > 0.2:
+		var look_direction = Vector2(_velocity.z, _velocity.x)
+		_model.rotation.y = look_direction.angle()
